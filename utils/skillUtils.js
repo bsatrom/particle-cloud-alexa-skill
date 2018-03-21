b4fc702e3e98f8a26eba1f2862c89683fbde95e9
Brandon Satrom <bsatrom@gmail.com>
2018-03-21 09:44:43 -0500

    added variable fns

--- utils/skillUtils.js
+++ utils/skillUtils.js
@@ -1,19 +1,21 @@
 // Utility functions for Particle Alexa Skill
 
-const normalizeDeviceName = (name) => {
+const normalizeDeviceName = name => {
   return name ? name.toLowerCase().replace(/[-_]+/g, ' ') : '';
 };
 
-const normalizeFunctionName = (name) => {
+const normalizeFunctionName = name => {
   const functionWords = name.split(' ');
 
-  let normalizedName = functionWords.map((word, index) => {
-    if (index === 0) {
-      return word;
-    } else {
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    }
-  }).join('');
+  let normalizedName = functionWords
+    .map((word, index) => {
+      if (index === 0) {
+        return word;
+      } else {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }
+    })
+    .join('');
 
   return normalizedName;
 };
@@ -22,20 +24,29 @@ const sayArray = (items, penultimateWord = 'and') => {
   let result = '';
 
   items.forEach((element, index, arr) => {
-      
-      if (index === 0) {
-          result = element;
-      } else if (index === items.length - 1) {
-          result += ` ${penultimateWord} ${element}`;
-      } else {
-          result += `, ${element}`;
-      }
+    if (index === 0) {
+      result = element;
+    } else if (index === items.length - 1) {
+      result += ` ${penultimateWord} ${element}`;
+    } else {
+      result += `, ${element}`;
+    }
   });
   return result;
+};
+
+const sayObject = items => {
+  return sayArray(Object.keys(items));
+};
+
+const objectIsEmpty = object => {
+  return Object.keys(object).length === 0 && object.constructor === Object;
 };
 
 exports.utils = {
   normalizeDeviceName,
   normalizeFunctionName,
-  sayArray
+  sayArray,
+  sayObject,
+  objectIsEmpty
 };
